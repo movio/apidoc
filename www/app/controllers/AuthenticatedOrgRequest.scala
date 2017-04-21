149128d6bcc06fa4450f7b4a8a6605f0f1b5abd7
Jerry Peng <jerry@movio.co>
2017-04-21 15:28:33 +1200

    Extract org keys correctly

--- www/app/controllers/AuthenticatedOrgRequest.scala
+++ www/app/controllers/AuthenticatedOrgRequest.scala
@@ -60,7 +60,7 @@ object AuthenticatedOrg extends ActionBuilder[AuthenticatedOrgRequest] {
       }
     }
 
-    val orgKeyOption = request.path.split("/").drop(1).headOption
+    val orgKeyOption = request.path.replace(routes.ApplicationController.index().path, "").split("/").drop(1).headOption
 
     request.session.get("user_guid").map { userGuid =>
 
